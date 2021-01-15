c18ab138ce14d8dd86b237140d9be94d0d697d0e
Elmar Beckmann <elmar@openweb.nl>
2021-01-15 12:20:14 +0100

    initial commit

---
+++ apps/elmardotdev/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elmardotdev-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'elmardotdev';
+}
